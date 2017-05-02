6862ec53e547d6d824f6051de87d0da8fd2b7e95
Sam Abradi <sabradi@vivox.com>
2017-05-02 11:18:18 -0400

    why GMT, couldn't you have just been localtime?

--- src/lager_rsyslog_util.erl
+++ src/lager_rsyslog_util.erl
@@ -162,7 +162,7 @@ num_to_month(N) ->
     end.                          
 
 syslog_ng_compat_timestamp() ->
-    {{_Year,Month,Day},{Hour,Minute,Second}} = calendar:now_to_datetime(os:timestamp()),
+    {{_Year,Month,Day},{Hour,Minute,Second}} = calendar:localtime(),
     Format = "~3s ~2.10.0B ~2.10.0B:~2.10.0B:~2.10.0B",
     io_lib:format(Format, [num_to_month(Month), Day, Hour, Minute, Second]).
 
